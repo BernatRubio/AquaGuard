ef860573e37a2f33322f541d15fc710b5b360d48
BernatRubio <bernat.rubio.oliete@gmail.com>
2025-05-10 13:00:29 +0200

    Segment time must be in minutes!

--- AquaGuard Watch App/Core/Algorithm/TissueGasSimulator.swift
+++ AquaGuard Watch App/Core/Algorithm/TissueGasSimulator.swift
@@ -15,7 +15,8 @@ struct TissueGasSimulator {
         let oldPressure = session.currentPressure.converted(to: .bars).value
         guard let newPressure = measurement.pressure?.converted(to: .bars).value else { return }
         let segmentTime = (measurement.date.timeIntervalSince(session.currentTime) * 1000).rounded() / 1000 // Approx. 0.333 seconds if called 3 times per second
-        guard segmentTime > 0 else { return }
+        let segmentTimeInMinutes = segmentTime / 60
+        guard segmentTimeInMinutes > 0 else { return }
         
         for i in 0..<ZHL16_N2.count {
             let nitrogenPressure = session.compartments[i].nitrogen.pressure.converted(to: .bars).value
@@ -25,8 +26,8 @@ struct TissueGasSimulator {
             let pn = schreinerEquation(
                 Pi: nitrogenPressure,
                 Palv: calculateAlveolarPressure(Pamb: newPressure, Q: nitrogenPercentage, RQ: respiratoryQuotient),
-                t: segmentTime,
-                R: calculateGasRate(d0: oldPressure, dt: newPressure, t: segmentTime, Q: nitrogenPercentage),
+                t: segmentTimeInMinutes,
+                R: calculateGasRate(d0: oldPressure, dt: newPressure, t: segmentTimeInMinutes, Q: nitrogenPercentage),
                 k: decayConstant(fromHalfTime: session.compartments[i].nitrogen.halfTime)
             )
             
@@ -38,8 +39,8 @@ struct TissueGasSimulator {
             let ph = schreinerEquation(
                 Pi: heliumPressure,
                 Palv: calculateAlveolarPressure(Pamb: newPressure, Q: heliumPercentage, RQ: respiratoryQuotient),
-                t: segmentTime,
-                R: calculateGasRate(d0: oldPressure, dt: newPressure, t: segmentTime, Q: heliumPercentage),
+                t: segmentTimeInMinutes,
+                R: calculateGasRate(d0: oldPressure, dt: newPressure, t: segmentTimeInMinutes, Q: heliumPercentage),
                 k: decayConstant(fromHalfTime: session.compartments[i].helium.halfTime)
             )
             
